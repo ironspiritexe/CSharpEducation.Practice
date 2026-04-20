2337a9dcb20d91e41294daf6eee09663c469d065
ironspiritexe <ironspirit.exe@gmail.com>
2026-04-20 13:56:50 +0400

    Задание 4

---
+++ Practice1/Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите фразу: "); // Выводим сообщение с просьбой ввести первую фразу
+        string phrase1; // Объявляем переменную для первой фразы
+        phrase1 = Console.ReadLine(); // Считываем и записываем первую фразу
+        Console.Write("Введите вторую фразу: "); // Выводим сообщение с просьбой ввести вторую фразу
+        string phrase2; // Объявляем переменную для второй фразы
+        phrase2 = Console.ReadLine(); // Считываем и записываем вторую фразу
+        Console.WriteLine(phrase1 + " " + phrase2); // Выводим первую и вторую фразу 
+    }
+}
